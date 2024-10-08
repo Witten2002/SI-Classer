25a08eba9981fbc70728ed75564f2fbac7d00404
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-10-08 10:44:43 +0200

    Person exercise

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+/**
+ * This is the class for Person
+ *
+ * @author Ludwig Wittenberg <lw223cq@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// -----------------------------------------
+// TODO:
+//    Create a calss called Person.
+//    Create the constructor with name and age.
+//    Create a method called greet. That should write out in the console your name and age.
+//    Export the class.
+// -----------------------------------------
---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * This is the start point of the application
+ *
+ * @author Ludwig Wittenberg <lw223cq@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// -----------------------------------------
+// TODO:
+//    Import the class Person.
+//    Create a new instace of the Person
+//    Call the method greet.
+// -----------------------------------------
