dc5441136cccdacfd2ebfdfd6cd427f54f59799f
Tiberius Gherac <tg222hh@student.lnu.se>
2024-10-11 14:15:52 +0200

    S

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@
 
 import { Person } from './Person.js'
 
+/**
+ * An instance of the Person class.
+ * 
+ * @type {Person}
+ * @constant
+ */
 const instanceOfPerson = new Person ('Tiberius', 28)
 
 instanceOfPerson.greet()
+instanceOfPerson.singLorem()
+
